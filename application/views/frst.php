551656997ab5346d36aa746097924ce4f538a7ca
Tasneem <Tasneem>
2017-03-12 23:34:53 +0530

    abc

---
+++ application/views/frst.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link href= "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css"  
+	  rel="stylesheet"/>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-materialize/0.2.2/angular-materialize.min.js">
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"> </script>
+
+</head>
+
+<body class="container">
+<nav>
+<div class="navbar-wrapper container">
+<a href="#" class="brand-logo"> YEAR </a>
+</div>
+</nav>
+
+<div class="card-panel">
+        
+    <a class="btn-floating btn-large waves-effect waves-light blue"><i class="material-icons"> 1 </i></a>
+
+    </div>  <div class="card-panel">
+    <a class="btn-floating btn-large waves-effect waves-light blue"><i class="material-icons"> 2 </i></a>
+    </div>  <div class="card-panel">
+
+    <a class="btn-floating btn-large waves-effect waves-light blue"><i class="material-icons"> 3 </i></a>
+    </div>  <div class="card-panel">
+
+    <a class="btn-floating btn-large waves-effect waves-light blue"><i class="material-icons"> 4 </i></a>
+    </div>     
+
+
+
+
+</body>
+</html>
---
+++ application/views/proctor_mgnt.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link href= "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css"  
+	  rel="stylesheet"/>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-materialize/0.2.2/angular-materialize.min.js">
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"> </script>
+
+</head>
+<body class="container" bgcolor="white">
+<nav class="#ef5350 red lighten-1">        
+<div class="navbar-wrapper container">
+<a href="#" class="brand-logo center">PROCTOR MANAGEMENT SYSTEM</a>
+</div>
+</nav>
+<br />
+<br />
+<div class="divider"></div>
+<!--STUDENT LIST-->
+       <div id="STUDENT LIST" class="section">
+              <div class="row">
+                <div class="col s6 m6 l6">
+                  <div class="card-panel">
+                    <h4 class="header2">STUDENT LIST</h4>
+                      <div class="row">
+                          <div class="input-field col s12">
+                            <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Search">
+                            
+                          </div>
+                         </div>
+                      </form>
+                    </div>
+                  </div> 
+
+<!--PROCTOR LIST-->
+       <div id="PROCTOR LIST" class="section">
+              <div class="row">
+                <div class="col s6 m6 l6">
+                  <div class="card-panel">
+                    <h4 class="header2">PROCTOR LIST</h4>
+                      
+                         <div class="row">
+                          <div class="input-field col s12">
+                            <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Search">
+                            
+                          </div>
+                         </div>
+                      </form>
+                    </div>
+                  </div>
